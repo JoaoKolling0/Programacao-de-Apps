bd87735bf7a456b7c8f427e59a2590904b7ecea5
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-06-04 15:05:43 -0300

    Atv 03 atualizada

--- Atv 03/locacao_carros.py
+++ Atv 03/locacao_carros.py
@@ -1,12 +1,12 @@
 class Veiculo: #Classe que vai englobar as coisas sobre os Veiculos.
-    def __init__(self):
-        self.carro
-        self.preco
-        self.data
-        self.quilometragem
+    def __init__(self, modelo, preco, ano, quilometragem):
+        self.modelo = modelo
+        self.preco = preco
+        self.ano = ano
+        self.quilometragem = quilometragem
+    
 
-    def modelos(self):
-        pass
 
 if __name__ == "__main__":
-    pass
+    objeto_veiculo = Veiculo(modelo = "Saveiro", preco = 400, ano = 2012, quilometragem = 1200)
+    print(objeto_veiculo.modelo, objeto_veiculo.preco, objeto_veiculo.ano, objeto_veiculo.quilometragem) 
