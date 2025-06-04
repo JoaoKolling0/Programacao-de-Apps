465ef35d159076041c8b19f7400897529320ae90
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-06-04 14:28:01 -0300

    Atv 3 criada

---
+++ Atv 03/.dist/locacao_carros.py
@@ -0,0 +1,11 @@
+class Veiculo: #Classe que vai englobar as coisas sobre os Veiculos.
+    def __init__(self):
+        self.carro
+        self.preco
+        self.data
+        self.quilometragem
+
+
+
+if __name__ == "__main__":
+    pass
