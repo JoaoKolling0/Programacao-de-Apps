418db76957baa263a124d66eadce40a71576c4d9
JoaoKolling0 <joao.victhor.kolling@gmail.com>
2025-06-06 16:03:36 -0300

    Atv 04 - atualizado

--- Atv 04/gerenciador_arquivos.py
+++ Atv 04/gerenciador_arquivos.py
@@ -8,11 +8,43 @@ class Arquivo:
         self.tamanho = tamanho
         
 class Diretorio:
-    def __init__(self, nome, lista_arquivo, lista_pasta):
+    def __init__(self, nome):
         
         self.nome = nome
-        self.lista_arquivo = lista_arquivo
-        self.lista_pasta = lista_pasta
+        self.lista_arquivo = [] #Colchetes usados para criar uma lista.
+        self.lista_pasta = [] #Colchetes usados para criar uma lista.
         
+    def add_arquivo(self, objeto_arquivo):
+        self.lista_arquivo.append(objeto_arquivo)
+        
+    def add_pasta(self, objeto_pasta):
+        self.lista_pasta.append(objeto_pasta)
+        
+    def listar_conteudo(self):
+        print("Arquivos -", self.nome)
+        for arquivo in self.lista_arquivo:
+            print(arquivo.nome)
+        
+           
+    
 if __name__ == "__main__":
-    pass
+    
+    print("")
+    
+    objeto_arquivo01 = Arquivo(nome = "- ACDC - Back in Black -", tipo = "mp3 -", data = "06/062/025 -", tamanho = "6mb -")
+    print(objeto_arquivo01.nome, objeto_arquivo01.tipo, objeto_arquivo01.data, objeto_arquivo01.tamanho)
+    
+    print("")
+    
+    objeto_arquivo02 = Arquivo(nome = "- SURVIVOR - Eye Of The Tiger -", tipo = "mp3 -", data = "06/06/2025 -", tamanho = "6mb -")
+    print(objeto_arquivo02.nome, objeto_arquivo02.tipo, objeto_arquivo02.data, objeto_arquivo02.tamanho)
+    
+    print("")
+    
+    objeto_pasta01 = Diretorio(nome = "Músicas")
+    objeto_pasta01.add_arquivo(objeto_arquivo01)
+    objeto_pasta01.add_arquivo(objeto_arquivo02)
+    
+    objeto_pasta01.listar_conteudo()
+    
+    
