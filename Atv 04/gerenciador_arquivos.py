bc3bd0288af15b0c972d15a8df4c15452279b424
JoaoKolling0 <joao.victhor.kolling@gmail.com>
2025-06-06 14:35:07 -0300

    Atv 04 atualizada

--- Atv 04/gerenciador_arquivos.py
+++ Atv 04/gerenciador_arquivos.py
@@ -1,3 +1,18 @@
 # O objetivo da Atv 04 é fazer um simulador de diretórios (diretórios +- = a "pastas");
 class Arquivo:
+    def __init__(self, nome, tipo, data, tamanho):
+        
+        self.nome = nome
+        self.tipo = tipo
+        self.data = data
+        self.tamanho = tamanho
+        
+class Diretorio:
+    def __init__(self, nome, lista_arquivo, lista_pasta):
+        
+        self.nome = nome
+        self.lista_arquivo = lista_arquivo
+        self.lista_pasta = lista_pasta
+        
+if __name__ == "__main__":
     pass
