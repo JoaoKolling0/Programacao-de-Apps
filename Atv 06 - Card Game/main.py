b797a92652e98984f66c2ced284c0e3bc8a6e2f4
JoaoKolling0 <joao.victhor.kolling@gmail.com>
2025-06-11 16:53:33 -0300

    Atv 06 atualizada

--- Atv 06 - Card Game/main.py
+++ Atv 06 - Card Game/main.py
@@ -3,3 +3,4 @@ from personagem import Personagem
 objeto_personagem1 = Personagem("Kolling")
 mensagem = objeto_personagem1.falar_nome()
 print(mensagem)
+
--- Atv 06 - Card Game/personagem.py
+++ Atv 06 - Card Game/personagem.py
@@ -3,7 +3,15 @@ class Personagem:
         self.nome = nome
         self.vida_max = 100
         self.vida = 100
+        self.pontos_ataque = 8
+        self.pontos_defesa = 2
+        self.mao_de_cartas = []
+        self.energia_max = 10
+        self.energia = 10
         
     def falar_nome(self):
         return f"Olá, meu nome é {self.nome}"
     
+class Cartas:
+    def __init__(self, ver_lista_cartas):
+        self.ver_lista_cartas = ver_lista_cartas
